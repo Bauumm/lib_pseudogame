fb77f5afe74a9935081f8deb3011a151d7a25802
Bauumm <oh4@web.de>
2023-04-22 22:42:14 +0200

    optimize headless replays

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -25,17 +25,19 @@ u_execScript("game/game.lua")
 
 --- this function hides the default game components using a shader and default functions
 function PseudoGame.hide_default_game()
-	local nothing_shader = shdr_getDependencyShaderId("library_pseudogame", "pseudogame", "Baum", "nothing.frag")
-	if nothing_shader < 0 then
-		error("Failed loading shader!")
+	if not u_isHeadless() then
+		local nothing_shader = shdr_getDependencyShaderId("library_pseudogame", "pseudogame", "Baum", "nothing.frag")
+		if nothing_shader < 0 then
+			error("Failed loading shader!")
+		end
+		shdr_setActiveFragmentShader(RenderStage.BACKGROUNDTRIS, nothing_shader)
+		shdr_setActiveFragmentShader(RenderStage.PLAYERTRIS, nothing_shader)
+		shdr_setActiveFragmentShader(RenderStage.PLAYERTRIS3D, nothing_shader)
+		shdr_setActiveFragmentShader(RenderStage.CAPTRIS, nothing_shader)
+		shdr_setActiveFragmentShader(RenderStage.PIVOTQUADS, nothing_shader)
+		shdr_setActiveFragmentShader(RenderStage.PIVOTQUADS3D, nothing_shader)
+		shdr_setActiveFragmentShader(RenderStage.WALLQUADS3D, nothing_shader)
+		l_setShowPlayerTrail(false)
+		l_setShadersRequired(true)
 	end
-	shdr_setActiveFragmentShader(RenderStage.BACKGROUNDTRIS, nothing_shader)
-	shdr_setActiveFragmentShader(RenderStage.PLAYERTRIS, nothing_shader)
-	shdr_setActiveFragmentShader(RenderStage.PLAYERTRIS3D, nothing_shader)
-	shdr_setActiveFragmentShader(RenderStage.CAPTRIS, nothing_shader)
-	shdr_setActiveFragmentShader(RenderStage.PIVOTQUADS, nothing_shader)
-	shdr_setActiveFragmentShader(RenderStage.PIVOTQUADS3D, nothing_shader)
-	shdr_setActiveFragmentShader(RenderStage.WALLQUADS3D, nothing_shader)
-	l_setShowPlayerTrail(false)
-	l_setShadersRequired(true)
 end
