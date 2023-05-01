c6f552f3c85066845e90b6162ebf8a950c9b15b5
Bauumm <105805045+Bauumm@users.noreply.github.com>
2023-05-01 21:32:44 +0200

    Fix

--- Scripts/game/common.lua
+++ Scripts/game/common.lua
@@ -24,7 +24,7 @@ function PseudoGame.game.get_color_from_hue(hue)
     local q = 1 - f
     local t = f
 
-    function ret(r, g, b)
+    local function ret(r, g, b)
         return { r * 255, g * 255, b * 255, 255 }
     end
 
